33fd2a9ca72e964d142f90f36ca142a1dbaa407e
Pazaz <pazaz@itspazaz.com>
2024-06-24 03:14:20 -0400

    feat: Expanded RedundantGotoTransformer cases

--- deobfuscator/src/main/java/org/runewiki/asm/AsmUtil.java
+++ deobfuscator/src/main/java/org/runewiki/asm/AsmUtil.java
@@ -1,8 +1,10 @@
 package org.runewiki.asm;
 
+import java.util.*;
+import java.util.function.Predicate;
+
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
 
 public class AsmUtil {
     public static AbstractInsnNode getNextReal(AbstractInsnNode insn) {
@@ -24,4 +26,58 @@ public class AsmUtil {
     public static boolean hasCode(MethodNode method) {
         return (method.access & (Opcodes.ACC_NATIVE | Opcodes.ACC_ABSTRACT)) == 0;
     }
+
+    public static boolean isTerminal(AbstractInsnNode insn) {
+        return insn.getOpcode() == Opcodes.GOTO ||
+               insn.getOpcode() == Opcodes.RETURN ||
+               insn.getOpcode() == Opcodes.ARETURN ||
+               insn.getOpcode() == Opcodes.IRETURN ||
+               insn.getOpcode() == Opcodes.LRETURN ||
+               insn.getOpcode() == Opcodes.FRETURN ||
+               insn.getOpcode() == Opcodes.DRETURN ||
+               insn.getOpcode() == Opcodes.ATHROW ||
+               insn.getOpcode() == Opcodes.TABLESWITCH ||
+               insn.getOpcode() == Opcodes.LOOKUPSWITCH;
+    }
+
+    public static int removeIf(InsnList instructions, Predicate<AbstractInsnNode> condition) {
+        AbstractInsnNode insn = instructions.getFirst();
+
+        int count = 0;
+        while (insn != null) {
+            AbstractInsnNode next = insn.getNext();
+
+            if (condition.test(insn)) {
+                instructions.remove(insn);
+                count++;
+            }
+
+            insn = next;
+        }
+
+        return count;
+    }
+
+    public static List<LabelNode> getJumpTargets(AbstractInsnNode insn) {
+        if (insn instanceof JumpInsnNode) {
+            return List.of(((JumpInsnNode) insn).label);
+        }
+
+        if (insn instanceof TableSwitchInsnNode) {
+            ArrayList result = new ArrayList<LabelNode>(((TableSwitchInsnNode) insn).labels.size() + 1);
+            result.addAll(((TableSwitchInsnNode) insn).labels);
+            result.add(((TableSwitchInsnNode) insn).dflt);
+            return result;
+        }
+
+        if (insn instanceof LookupSwitchInsnNode) {
+            ArrayList result = new ArrayList<LabelNode>(((LookupSwitchInsnNode) insn).labels.size() + 1);
+            result.addAll(((LookupSwitchInsnNode) insn).labels);
+            result.add(((LookupSwitchInsnNode) insn).dflt);
+            return result;
+        }
+
+        return List.of();
+    }
+
 }
--- deobfuscator/src/main/java/org/runewiki/deob/bytecode/transform/RedundantGotoTransformer.java
+++ deobfuscator/src/main/java/org/runewiki/deob/bytecode/transform/RedundantGotoTransformer.java
@@ -1,43 +1,151 @@
 package org.runewiki.deob.bytecode.transform;
 
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.JumpInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
 import org.runewiki.asm.AsmUtil;
 import org.runewiki.asm.transform.Transformer;
 
-import java.util.List;
+import java.util.*;
 
 /*
  * Remove redundant GOTO instructions
  */
 public class RedundantGotoTransformer extends Transformer {
-    private int removed = 0;
+    private int removedGoto = 0;
+    private int removedLabel = 0;
 
     @Override
     public void preTransform(List<ClassNode> classes) {
-        this.removed = 0;
+        this.removedGoto = 0;
+        this.removedLabel = 0;
     }
 
     @Override
     public boolean transformCode(List<ClassNode> classes, ClassNode clazz, MethodNode method) {
-        for (AbstractInsnNode insn : method.instructions) {
-            if (
-                insn.getOpcode() == Opcodes.GOTO &&
-                AsmUtil.getNextReal(insn) == AsmUtil.getNextReal(((JumpInsnNode) insn).label)
-            ) {
-                method.instructions.remove(insn);
-                this.removed++;
-            }
+        // todo: remove unreachable code first?
+
+        if (method.tryCatchBlocks.isEmpty()) {
+            sortBlocks(method);
         }
+
+        removeGotoNext(method);
+        removeUnusedLabels(method);
 
         return false;
     }
 
     @Override
     public void postTransform(List<ClassNode> classes) {
-        System.out.println("Removed " + this.removed + " redundant GOTO instructions");
+        System.out.println("Removed " + this.removedGoto + " redundant GOTO instructions, " + this.removedLabel + " unused labels");
+    }
+
+    private void removeGotoNext(MethodNode method) {
+        this.removedGoto += AsmUtil.removeIf(method.instructions, insn -> insn instanceof JumpInsnNode && insn.getOpcode() == Opcodes.GOTO && ((JumpInsnNode) insn).label == insn.getNext());
+    }
+
+    private void removeUnusedLabels(MethodNode method) {
+        HashSet usedLabels = new HashSet<LabelNode>();
+
+        for (TryCatchBlockNode tryCatch : method.tryCatchBlocks) {
+            usedLabels.add(tryCatch.start);
+            usedLabels.add(tryCatch.end);
+            usedLabels.add(tryCatch.handler);
+        }
+
+        for (AbstractInsnNode insn : method.instructions) {
+            if (insn instanceof JumpInsnNode) {
+                usedLabels.add(((JumpInsnNode) insn).label);
+            }
+
+            if (insn instanceof TableSwitchInsnNode) {
+                usedLabels.add(((TableSwitchInsnNode) insn).dflt);
+                usedLabels.addAll(((TableSwitchInsnNode) insn).labels);
+            }
+
+            if (insn instanceof LookupSwitchInsnNode) {
+                usedLabels.add(((LookupSwitchInsnNode) insn).dflt);
+                usedLabels.addAll(((LookupSwitchInsnNode) insn).labels);
+            }
+        }
+
+        this.removedLabel += AsmUtil.removeIf(method.instructions, insn -> insn instanceof LabelNode && !usedLabels.contains((LabelNode) insn));
+    }
+
+    private void sortBlocks(MethodNode method) {
+        MethodNode newInstructions = new MethodNode();
+        List<Block> sortedBlocks = sortBlocks(method.instructions);
+
+        for (Block block : sortedBlocks) {
+            if (block.instructions.isEmpty() || !(block.instructions.get(0) instanceof LabelNode)) {
+                block.instructions.add(0, new LabelNode());
+            }
+        }
+
+        for (Block block : sortedBlocks) {
+            if (!AsmUtil.isTerminal(block.instructions.get(block.instructions.size() - 1))) {
+                block.instructions.add(new JumpInsnNode(Opcodes.GOTO, ((LabelNode) block.next.instructions.get(0))));
+            }
+        }
+
+        for (Block block : sortedBlocks) {
+            for (AbstractInsnNode insn : block.instructions) {
+                insn.accept(newInstructions);
+            }
+        }
+
+        method.instructions = newInstructions.instructions;
+    }
+
+    private List<Block> sortBlocks(InsnList code) {
+        Block startBlock = new Block();
+        Map<LabelNode, Block> labelBlocks = new HashMap<>();
+        Block currentBlock = startBlock;
+
+        for (AbstractInsnNode insn : code) {
+            if (insn instanceof LabelNode) {
+                Block nextBlock = labelBlocks.computeIfAbsent((LabelNode) insn, l -> new Block());
+
+                if (currentBlock.instructions.isEmpty() || !AsmUtil.isTerminal(currentBlock.instructions.get(currentBlock.instructions.size() - 1))) {
+                    currentBlock.next = nextBlock;
+                    nextBlock.last = currentBlock;
+                }
+
+                currentBlock = nextBlock;
+            }
+
+            currentBlock.instructions.add(insn);
+
+            for (LabelNode target : AsmUtil.getJumpTargets(insn)) {
+                currentBlock.jumpTargets.add(labelBlocks.computeIfAbsent(target, k -> new Block()));
+            }
+        }
+
+        List<Block> result = new ArrayList<>();
+        sort(result, new HashSet<>(), startBlock);
+        Collections.reverse(result);
+        return result;
+    }
+
+    private void sort(List<Block> result, Set<Block> visited, Block block) {
+        if (!visited.add(block)) {
+            return;
+        }
+
+        for (Block jumpTarget : block.jumpTargets) {
+            sort(result, visited, jumpTarget);
+        }
+
+        if (block.next != null) {
+            sort(result, visited, block.next);
+        }
+
+        result.add(block);
+    }
+
+    private static class Block {
+        public List<AbstractInsnNode> instructions = new ArrayList<>();
+        public List<Block> jumpTargets = new ArrayList<>();
+        public Block last;
+        public Block next;
     }
 }
